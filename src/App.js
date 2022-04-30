af01d3ddc66606b30dc8f2e044c8b4a06193f9f5
NavrozashviliDamiane <104000748+NavrozashviliDamiane@users.noreply.github.com>
2022-04-30 18:53:08 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import Burgers from './components/Burgers/Burgers';
